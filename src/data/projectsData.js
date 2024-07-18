d511d3258c11f231c851491f77ac5f755ba4d580
sharmani <sharmani@iu.edu>
2024-07-18 00:42:19 -0400

    commit

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,4 +1,5 @@
 
+import four from '../assets/svg/projects/four.svg'
 
 export const projectsData = [
     {
@@ -6,55 +7,55 @@ export const projectsData = [
         projectName: 'Healthcare Management System',
         projectDesc: "Crafted and launched a HIPAA-compliant Appointment Management System featuring advanced access control",
         tags: ['Asp.net core', "REST API", "MSSQL", "Vue.js"],
-        image: ' '
+        image: four
     },
     {
         id: 2,
         projectName: 'Healthy Hoosiers',
         projectDesc: 'Engineered a MERN stack wellness app on AWS EC2, boosting engagement with personalized plans and scalable content access.',
         tags: ['React', 'Tailwind CSS', "Express", "AWS", "MongoDB"],
-        code: 'https://github.com/dineshmannari/HealthyHoosiers',
-        image: ' '
+        code: 'https://github.com/anirudhsharma1007/HealthyHoosiers',
+        image: four
     },
     {
         id: 3,
         projectName: 'FocusSync',
         projectDesc: 'An App developed to support a wearable where the user can focus on day-day to task.',
         tags: ['Kotlin', 'XML', 'Firebase'],
-        code: 'https://github.com/dineshmannari/FocusSync',
-        image: ' '
+        code: 'https://github.com/anirudhsharma1007/FocusSync',
+        image: four
     },
     {
         id: 4,
         projectName: 'Movie Recommender System',
         projectDesc: 'Developed a graph neural network-based movie recommender, enhancing user experience with personalized, intelligent film suggestions.',
         tags: ['Python', 'Graph Neural network', 'PyGeometry'],
-        code: 'https://github.com/dineshmannari/ML-projects',
-        image: ''
+        code: 'https://github.com/anirudhsharma1007/ML-projects',
+        image: four
     },
     {
         id: 5,
         projectName: 'Image Captioning tool',
         projectDesc: 'Crafted an image captioning tool using autoencoder and LSTM, generating descriptive captions with precision and context-awareness.',
         tags: ['Python', 'Keras','Tensorflow'],
-        code: 'https://github.com/dineshmannari/Image-captioning-tool',
-        image: ''
+        code: 'https://github.com/anirudhsharma1007/Image-captioning-tool',
+        image: four
     },
     {
         id: 7,
         projectName: 'Feature Selection library',
         projectDesc: 'Developed a feature selection library using correlation-based clustering, outperforming others with reduced dimensionality and computational overhead.',
         tags: ['Python', 'numpy','Scikit-learn', 'pandas'],
-        code: 'https://github.com/dineshmannari/FeatureSelectionLibrary',
-        image: ''
+        code: 'https://github.com/anirudhsharma1007/FeatureSelectionLibrary',
+        image: four
     },
     {
         id: 8,
         projectName: 'Used Car price prediction',
         projectDesc: 'A car prediction using Multiple linear regression algorithm',
         tags: ['Flutter', 'React'],
-        code: 'https://github.com/dineshmannari/ML-projects',
-        image: ''
+        code: 'https://github.com/anirudhsharma1007/ML-projects',
+        image: four
     },
 ]
 
